653b164f41f7a844c870613b81e2d1bfd4ac840e
Jack Guthrie <jackg3323@gmail.com>
2022-08-15 18:00:00 +0100

    Add form submit JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+const form = document.querySelector("form");
 
+form /
+  addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const submission = object.fromEntries(new FormData(event.target));
+  });
